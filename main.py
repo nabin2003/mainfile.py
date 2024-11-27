892097b4b19a06b7a9febc4edb7130acbbee5868
nabin2003 <sb13516@student.samk.fi>
2024-11-28 00:35:04 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import addition
+import subtraction
+
+print("Calculator Application")
+
+x = addition.add_two_numbers(5, 3)
+y = subtraction.subtract_two_numbers(10, 4)
+
+print(f"5 + 3 = {x}")
+print(f"10 - 4 = {y}")
